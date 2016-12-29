716d0bc7d222007fbf3a4aa36ee3a03a3db24a2f
YeisonGomez <yeigomez97@hotmail.com>
2016-12-29 01:36:13 -0500

    add library angular/material

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,12 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { MeCardComponent } from './me-card/me-card.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { SkillsComponent } from './skills/skills.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'skills',  component: HomeComponent },
-  { path: 'projects',  component: AboutComponent }
+  { path: '', redirectTo: '/skills', pathMatch: 'full' },
+  { path: 'skills',  component: SkillsComponent },
+  { path: 'projects',  component: ProjectsComponent }
  ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule }     from './app-routing.module';
 import { SkillsComponent } from './skills/skills.component';
 import { MeCardComponent } from './me-card/me-card.component';
 import { ProjectsComponent } from './projects/projects.component';
@@ -18,7 +20,9 @@ import { ProjectsComponent } from './projects/projects.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule.forRoot(),
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
