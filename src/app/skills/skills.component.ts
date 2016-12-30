ee491a8741c1fc8b8dfb26a36c158db177eb13d2
YeisonGomez <yeigomez97@hotmail.com>
2016-12-30 02:32:22 -0500

    add skills with platforms

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SkillsComponent implements OnInit {
 
-  constructor() { }
+	skills: any[];
 
-  ngOnInit() {
-  }
+	constructor() { 
+		this.skills = [
+			{ platform: "Web", skills: [
+				{ name: "HTML/CSS", lvl: 400 },
+				{ name: "Javascript", lvl: 650 },
+				{ name: "AngularJS 1", lvl: 650 },
+				{ name: "AngularJS 2", lvl: 450 }
+			]},
+			{ platform: "Backend", skills: [
+				{ name: "NodeJS", lvl: 550 },
+				{ name: ".NET/Asmx", lvl: 500 },
+				{ name: "Python/Flask", lvl: 350 }
+			]},
+			{ platform: "Móvil", skills: [
+				{ name: "Ionic 1", lvl: 630 }
+			]}	
+		];
+	}
+
+	ngOnInit() {
+	}
 
 }
