e62a34ec7f2493d5cd1793400ad25c95d8fd9278
YeisonGomez <yeigomez97@hotmail.com>
2016-12-30 04:36:51 -0500

    add projects

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -7,9 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectsComponent implements OnInit {
 
-  constructor() { }
+	projects: any[];
 
-  ngOnInit() {
-  }
+	constructor() { 
+		this.projects = [
+			{ 
+				icon: "http://chaira.udla.edu.co/Api/Resources/imgs/LogoUA.svg",
+				name: "Api de Chairá",
+				description: "Servicios web que permiten el acceso seguro a datos de usuarios de la Universidad de la Amazonia, por medio del protocolo estandar OAuth2.",
+				site: "http://chaira.udla.edu.co/api_dev/documentation#bienvenido"
+			},
+			{ 
+				icon: "http://chaira.udla.edu.co/Api/Resources/imgs/LogoUA.svg",
+				name: "Chairá para desarrolladores",
+				description: "Plataforma para la administración de aplicaciones o sitios web, que hagan uso de la Api de Chairá.",
+				site: "http://chaira.udla.edu.co/api_dev/"
+			},
+			{ 
+				icon: "https://lh3.googleusercontent.com/LfSp5CcK_nW9IZCZwBKtz29AEZjWDdbSPtW8FdXcKu78miMUxVPdSt8WaDy0D8dkL_o=w300-rw",
+				name: "Prego",
+				description: "Aplicación móvil para la automatización de grupos académicos y comunicación entre docentes y estudiantes de la Universidad de la Amazonia.",
+				site: "https://play.google.com/store/apps/details?id=com.ionic.prego146994"
+			},
+			{ 
+				icon: "http://chaira.udla.edu.co/Api/Resources/imgs/LogoUA.svg",
+				name: "UniAmazonia",
+				description: "Aplicación móvil para informar a la comunidad de la Universidad de la Amazonia de noticias y eventos relevantes, con su respectivo administrador web.",
+				site: "https://play.google.com/store/apps/details?id=com.ionicframework.udla&hl=es_419"
+			}
+		];
+	}
+
+	ngOnInit() {
+	}
 
 }
--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -13,17 +13,18 @@ export class SkillsComponent implements OnInit {
 		this.skills = [
 			{ platform: "Web", skills: [
 				{ name: "HTML/CSS", lvl: 400 },
-				{ name: "Javascript", lvl: 650 },
-				{ name: "AngularJS 1", lvl: 650 },
-				{ name: "AngularJS 2", lvl: 450 }
+				{ name: "Javascript", lvl: 650},
+				{ name: "AngularJS 1", lvl: 650, link: "https://angularjs.org/" },
+				{ name: "AngularJS 2", lvl: 500, link: "https://angular.io/docs/ts/latest/quickstart.html" }
 			]},
 			{ platform: "Backend", skills: [
-				{ name: "NodeJS", lvl: 550 },
-				{ name: ".NET/Asmx", lvl: 500 },
-				{ name: "Python/Flask", lvl: 350 }
+				{ name: "NodeJS", lvl: 550, link: "https://nodejs.org/es/" },
+				{ name: ".NET/Asmx", lvl: 500, link: "https://msdn.microsoft.com/en-us/library/ms972326.aspx" },
+				{ name: "Python/Flask", lvl: 350, link: "https://www.fullstackpython.com/flask.html" }
 			]},
 			{ platform: "Móvil", skills: [
-				{ name: "Ionic 1", lvl: 630 }
+				{ name: "Ionic 1", lvl: 630, link: "http://ionicframework.com/docs/overview/" },
+				{ name: "Swift", lvl: 80, link: "http://www.apple.com/co/swift/" }
 			]}	
 		];
 	}
